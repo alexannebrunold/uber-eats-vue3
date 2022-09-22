5d2f585839b4d809b35cbc2123c4faa321db21df
Alexanne Brunold <alexanne.brunold@hetic.net>
2022-09-22 10:42:56 +0200

    ADD Setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+bjr
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+@import url('../src/assets/style/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+#app {
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
